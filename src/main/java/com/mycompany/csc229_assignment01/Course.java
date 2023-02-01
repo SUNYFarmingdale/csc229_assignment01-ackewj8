9fa2b55483c44b4aa88c943efce5e9b0cd34cd3a
willi <willi@LAPTOP-9RHPACUH>
2023-02-01 17:02:40 -0500

    Lab 01 Assignment

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -33,5 +33,51 @@ package com.mycompany.csc229_assignment01;
  * @author MoaathAlrajab
  */
 public class Course {
-
+    // data members for Course
+    private int ID;
+    private String name;
+    private int code;
+    
+    // default constructor for Course
+    Course() {
+        ID = 0;
+        name = "X";
+        code = 0;
+    }
+    // overloaded constructor for Course
+    Course(int d, String n, int cd) {
+        ID = d;
+        name = n;
+        code = cd;
+    }
+    
+    // setter for ID
+    public void setID(int i) {
+        ID = i;
+    }
+    
+    // getter for ID
+    public int getID() {
+        return ID;
+    }
+    
+    // setter for name
+    public void setName(String n) {
+        name = n;
+    }
+    
+    // getter for name
+    public String getName() {
+        return name;
+    }
+    
+    // setter for code
+    public void setCode(int c) {
+        code = c;
+    }
+    
+    // getter for code
+    public int getCode() {
+        return code;
+    }
 }
--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -6,9 +6,27 @@ package com.mycompany.csc229_assignment01;
  * @author MoaathAlrajab
  */
 public class NewClass {
-    
      public static void main(String[] args) {
-        System.out.println("Hello World!");
+         // default constructor for Course
+        Course c = new Course();
+        
+        // setting the values (default constructor)
+        c.setID(214);
+        c.setName("John Doe");
+        c.setCode(713);
+        
+        // printing the values (default constructor)
+        System.out.println("Default ID: " + c.getID()); 
+        System.out.println("Default Name: " + c.getName()); 
+        System.out.println("Default Code: " + c.getCode()); 
+        
+        // overloaded constructor for Course
+        Course a = new Course(543, "Jane Doe", 925);
+        
+        // printing the values (overloaded constructor)
+        System.out.println("Overloaded ID: " + a.getID());
+        System.out.println("Overloaded Name: " + a.getName());
+        System.out.println("Overloaded Code: " + a.getCode());
     }
     
 }
